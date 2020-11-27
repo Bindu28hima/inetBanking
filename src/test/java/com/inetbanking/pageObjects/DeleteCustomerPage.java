afbf32bd16bd12f49af14aeaa48aae6fd9edb1ec
bindu <mbhbindu@gmail.com>
2020-11-27 17:01:40 +0530

    added delete customer page

--- src/test/java/com/inetbanking/pageObjects/DeleteCustomerPage.java
+++ src/test/java/com/inetbanking/pageObjects/DeleteCustomerPage.java
@@ -1,5 +1,53 @@
 package com.inetbanking.pageObjects;
 
-public class DeleteCustomerPage {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.inetbanking.testCases.BaseClass;
+
+public class DeleteCustomerPage extends BaseClass {
+
+
+	WebDriver ldriver;
+
+	public DeleteCustomerPage(WebDriver rdriver) {
+		ldriver=rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
+
+	@FindBy(xpath = "//a[text()='Delete Customer']")
+	WebElement deleteCustomer;
+
+	@FindBy(name = "cusid")
+	WebElement customerId;
+
+	@FindBy(name = "AccSubmit")
+	WebElement submitBtn;
+
+	public void clickDeleteCustomer() 
+	{
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeScript("arguments[0].click();", deleteCustomer);
+	}
+
+	public void enterCustomerId(String ID)
+	{
+		customerId.sendKeys(ID);
+	}
+
+	public void clickSubmitBtn()
+	{
+		submitBtn.click();
+	}
+
+	public void alert() 
+	{
+		Alert alt =driver.switchTo().alert();
+		alt.accept();
+
+	}
 }
--- src/test/java/com/inetbanking/testCases/DeleteCustomerTest.java
+++ src/test/java/com/inetbanking/testCases/DeleteCustomerTest.java
@@ -1,8 +1,33 @@
 package com.inetbanking.testCases;
 
-public class DeleteCustomerTest {
-	
+import org.testng.annotations.Test;
 
-	
-	
+import com.inetbanking.pageObjects.DeleteCustomerPage;
+import com.inetbanking.pageObjects.LoginPage;
+
+public class DeleteCustomerTest extends BaseClass{
+
+	@Test
+	public void deleteCustomer() throws InterruptedException
+	{
+
+
+		LoginPage lp=new LoginPage(driver);
+		lp.setUserName(username);
+		logger.info("User name is provided");
+		lp.setPassword(password);
+		logger.info("Passsword is provided");
+		lp.clickSubmit();
+
+		DeleteCustomerPage deletecust = new DeleteCustomerPage(driver);
+
+		deletecust.clickDeleteCustomer();
+		deletecust.enterCustomerId("66568");
+		deletecust.clickSubmitBtn();
+		deletecust.alert();
+		
+		Thread.sleep(3000);
+
+
+	}
 }
